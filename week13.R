77cba736709b922bf24bb232f5e76af73359ed54
MarkU3U <mark900221@gmail.com>
2023-12-06 13:41:52 +0800

    1

---
+++ week13.R
@@ -0,0 +1,25 @@
+{
+  x+3
+}
+values_x<-C(2,4,7,15)
+seq_along(values_x)
+
+
+# oneComingStudentMajor == "Economics"
+print("Please go to room 201.")
+
+# oneComingStudentMajor == "Sociology"
+print("Please go to room 301.")
+
+Sociology
+
+"Economics"
+
+if(oneComingStudentMajor == "Economics"){
+  print("Please go to room 201.")
+}
+
+if(oneComingStudentMajor == "Sociology"){
+  print("Please go to room 301.")
+}
+
